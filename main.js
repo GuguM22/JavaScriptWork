a4b2453490f97d94a31f8e9249435c2c839bf272
GuguM22 <gugu.mkhwanazi40@gmail.com>
2024-06-05 11:14:00 +0200

    added buttons

---
+++ main.js
@@ -0,0 +1,24 @@
+// let quantity = 0;
+
+
+
+// function addQuantity(amount) {
+//     quantity += amount;
+//     document.getElementById('quantity').innerText = quantity;
+// }
+
+// function showQuantity() {
+//     alert(`Current quantity: ${quantity}`);
+// }
+
+
+// function clearCart() {
+//     quantity = 0;
+//     document.getElementById('quantity').innerText = quantity;
+// }
+
+// function removeItem() {
+//     quantity = 0;
+//     document.getElementById('quantity').innerText = quantity;
+//     document.getElementById('item').style.display = 'none';
+// }
